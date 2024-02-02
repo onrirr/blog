fd4704c12995f562bcaf7f7cf9f8f962535c2d60
onrirr <ulughann@gmail.com>
2024-02-02 23:43:04 +0300

    first commit

---
+++ tailwind.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from 'tailwindcss'
+import starlightPlugin from '@astrojs/starlight-tailwind'
+import colors from 'tailwindcss/colors'
+
+const config: Config = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        accent: colors.indigo,
+        gray: colors.zinc,
+      },
+    },
+  },
+  plugins: [starlightPlugin()],
+}
+
+export default config
